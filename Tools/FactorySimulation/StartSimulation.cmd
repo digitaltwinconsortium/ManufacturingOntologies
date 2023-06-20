2271965c7ebd7d9140b5af99894309c8b81becad
Erich Barnstedt <erichb@microsoft.com>
2023-06-20 12:29:45 +0200

    Add overwrite flag in case blob already exists.

--- Tools/FactorySimulation/StartSimulation.cmd
+++ Tools/FactorySimulation/StartSimulation.cmd
@@ -118,7 +118,7 @@ ECHO .
 ECHO Uploading UA-CloudPublisher config files to cloud...
 CD "C:\k8s\PublisherConfig\Munich\"
 CALL az storage container create -n munich --connection-string !storageconnectionstring!
-CALL az storage blob upload-batch -d munich --connection-string !storageconnectionstring! -s "." --destination-path "app/settings"
+CALL az storage blob upload-batch -d munich --connection-string !storageconnectionstring! -s "." --destination-path "app/settings" --overwrite
 
 ECHO Starting UA-CloudPublisher, UA-CloudCommander and MES to upload OPC UA cert to cloud...
 CD "C:\k8s\Deployment\Munich\"
@@ -153,7 +153,7 @@ ECHO .
 Echo Uploading UA-CloudPublisher config files to cloud...
 CD "C:\k8s\PublisherConfig\Seattle\"
 CALL az storage container create -n seattle --connection-string !storageconnectionstring!
-CALL az storage blob upload-batch -d seattle --connection-string !storageconnectionstring! -s "." --destination-path "app/settings"
+CALL az storage blob upload-batch -d seattle --connection-string !storageconnectionstring! -s "." --destination-path "app/settings" --overwrite
 
 ECHO Starting UA-CloudPublisher, UA-CloudCommander and MES to upload OPC UA cert to cloud...
 CD "C:\k8s\Deployment\Seattle\"

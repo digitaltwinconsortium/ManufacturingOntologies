1a0938563751d32ee5e666820c6a8d2349acef92
Erich Barnstedt <erichb@microsoft.com>
2023-07-12 10:25:38 +0200

    Hardening for AKS-EE install script.

--- Deployment/Bootstrap.ps1
+++ Deployment/Bootstrap.ps1
@@ -6,10 +6,10 @@ Start-Transcript "C:\Temp\Bootstrap.log"
 $ErrorActionPreference = "SilentlyContinue"
 
 # Install AZ CLI and AKS-EE
-$azcli = Start-Job {msiexec /i https://aka.ms/installazurecliwindows /qn}
-Wait-Job $azcli
-$k8s = Start-Job {msiexec /i https://aka.ms/aks-edge/k8s-msi /qn}
-Wait-Job $k8s
+msiexec /i https://aka.ms/installazurecliwindows /passive
+sleep 120
+msiexec /i https://aka.ms/aks-edge/k8s-msi /passive
+sleep 120
 
 # Enable Hyper-V feature
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart

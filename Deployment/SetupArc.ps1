7947c9de7aef041214fff75b7bc7b2f97536142d
Erich Barnstedt <erichb@microsoft.com>
2024-01-29 11:51:35 +0100

    Made AIO deployment instructions more detailed.

--- Deployment/SetupArc.ps1
+++ Deployment/SetupArc.ps1
@@ -1,4 +1,5 @@
-Install-Module Az.Resources -Repository PSGallery -Force -AllowClobber -ErrorAction Stop  
-Install-Module Az.Accounts -Repository PSGallery -Force -AllowClobber -ErrorAction Stop 
+Install-Module Az.Resources -Repository PSGallery -Force -AllowClobber -ErrorAction Stop
+Install-Module Az.Accounts -Repository PSGallery -Force -AllowClobber -ErrorAction Stop
 Install-Module Az.ConnectedKubernetes -Repository PSGallery -Force -AllowClobber -ErrorAction Stop
+Install-Module Az.CustomLocation -Repository PSGallery -Force -AllowClobber -ErrorAction Stop
 Connect-AksEdgeArc -JsonConfigFilePath .\aksedge-config.json
